09ef0b2c9c9529f94c4c418fe8895add55f5e900
heinop <pekka.heino@unikie.com>
2020-10-28 14:00:36 +0200

    Added user to local storage and logout functionality

--- src/App.js
+++ src/App.js
@@ -15,17 +15,33 @@ const App = () => {
     )
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('bloglistAppUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault();
 
     try {
       const user = await loginService.login({ username, password })
+      window.localStorage.setItem('bloglistAppUser', JSON.stringify(user))
       setUser(user)
       setUsername('')
       setPassword('')
     } catch (exception) {
       console.error('Error in login', exception)
     }
+  }
+
+  const handleLogout = (event) => {
+    event.preventDefault()
+
+    window.localStorage.removeItem('bloglistAppUser')
+    setUser(null)
   }
 
   const loginForm = () => (
@@ -59,7 +75,8 @@ const App = () => {
     <div>
       <h2>blogs</h2>
       <p>
-        {user.name} logged in
+        {user.name} logged in 
+        <button type="button" onClick={handleLogout}>logout</button>
       </p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
