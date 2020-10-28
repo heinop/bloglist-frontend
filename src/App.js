633770f9682b16274c9a09de0a44899a23c91ed9
heinop <pekka.heino@unikie.com>
2020-10-28 13:42:01 +0200

    Task 5.1: Added login functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,78 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+      setBlogs(blogs)
+    )
   }, [])
 
-  return (
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const user = await loginService.login({ username, password })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.error('Error in login', exception)
+    }
+  }
+
+  const loginForm = () => (
+    <div>
+      <h2>log in to application</h2>
+      <form onSubmit={handleLogin}>
+        <p>
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </p>
+        <p>
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </p>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  )
+
+  const showBlogs = () => (
     <div>
       <h2>blogs</h2>
+      <p>
+        {user.name} logged in
+      </p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+    </div>
+  )
+
+  return (
+    <div>
+      {user === null ?
+        loginForm() :
+        showBlogs()
+      }
     </div>
   )
 }
